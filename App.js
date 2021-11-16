456a33e13e8897f6539fe830c9930aa0bf25cfa1
Yousuf144 <82269216+Yousuf144@users.noreply.github.com>
2021-11-16 19:01:43 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import * as React from 'react';
+
+import AppRouter from './config/routes';
+function App() {
+ 
+   
+
+
+  return (
+    <div >
+       <AppRouter />
+
+ 
+    </div>
+  );
+}
+
+export default App;
